9f535e763c0ef2c0a94d9184884bbaacdd003a26
Lucas Lopes De Souza Guilha <82731221+medowlucas@users.noreply.github.com>
2025-02-20 21:49:37 -0300

    chore: add .env.example with configuration examples and dynamic variables in project

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Olá, eu sou Lucas!</h1>
-        <p>Bem-vindo à minha página pessoal construída com React!</p>
-        <p>Este projeto é um site sobre mim, onde você pode conhecer minhas habilidades e projetos.</p>
-      </header>
+    <div>
+      <h1>Olá, meu nome é {process.env.REACT_APP_NAME}</h1>
+      <p>Atualmente sou {process.env.REACT_APP_JOB_TITLE}</p>
+      <a href={process.env.REACT_APP_LINKEDIN} target="_blank" rel="noopener noreferrer">
+        Confira meu LinkedIn
+      </a>
     </div>
   );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+document.title = process.env.REACT_APP_NAME || 'Meu Site Pessoal';
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
